08f2f6c459dfe2151063c31a937c0bb3e8d2c29f
nkonshin <nkonshin@me.com>
2019-10-14 22:36:20 +0300

    Backup 14 Oct

--- test.js
+++ test.js
@@ -1,24 +1,31 @@
-var inputValue = document.querySelector("[name='arr']").value;
-        var resultDom = document.querySelector("#result");
+    // switch (num) {
+    //     case num < 5:
+    //         console.log(num, 'The digit is lower then 5');
+    //         break;
+    //     case num > 10:
+    //         console.log(num, 'The digit is lower then 10');
+    //         break;
+    //     case num > 15:
+    //         console.log(num, 'The digit is lower then 15');
+    //         break;
+    //     case 6:
+    //         console.log(num, 'Your number is 20!');
+    //         break;
+    //     case 23:
+    //         console.log(num, 'Your number is 23!');
+    //         break;
+    //     case num > 20:
+    //         console.log(num);
+    //         break;
+    // }
+//}
 
-        var increment = 0;
-        var array = inputValue.split(" ");
-
-        array.forEach(function (item, index) {
-            if (item % 5 == 0 && item % 9 != 0) {
-                increment++;
-            }
-        })
-        if (array.length % 2 == 0) {
-            array[array.length - 1] = increment;
-        } else {
-            array[array.length - 2] = increment;
-        }
-
-        //Если надо число
-        //resultDom.textContent = increment
-
-        // Если надо массив
-        resultDom.textContent = array;
-
-        console.log(resultDom.textContent);
+for (let num = 1; num < 25; num++) {
+if (num <=5) {
+    console.log('false')
+} else if (num >10) {
+    console.log('fffalse')
+} else {
+    console.log('true')
+};
+}
